2382d63f0a7d883c25b40a937a5d8f8f9214da2b
Ugurcan Kaya <49519312+ugurcankaya@users.noreply.github.com>
2020-11-20 20:43:43 +0300

    Create identityMatrix.py

---
+++ identityMatrix.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+x = np.array([[2104], [1416], [1534],[852]])
+X = np.column_stack((np.ones(len(x)), x))
+thetas = np.array([
+                   [-40, 200, -150], #theta zeros in order
+                   [0.25, 0.1, 0.4] #theta ones in order
+
+])
+
+
+predictions = np.dot(X, thetas)
+print(predictions.shape)
+print(predictions)
+
+
+
+#identity matrix
+I = np.identity(predictions.shape[1]) #number of columns
+np.dot(predictions, I) #same as predictions matrix
