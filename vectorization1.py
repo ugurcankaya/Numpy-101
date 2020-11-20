bd121e0591e64f7b58f8bc46f1e1c9b26317f736
Ugurcan Kaya <49519312+ugurcankaya@users.noreply.github.com>
2020-11-20 20:01:36 +0300

    Update vectorization1.py

--- vectorization1.py
+++ vectorization1.py
@@ -2,5 +2,5 @@ import numpy as np
 
 x = np.array([[2104], [1416], [1534],[852]])
 X = np.column_stack((np.ones(len(x)), x))
-thetas = np.array([[-40], [0.25] ])
-xpredictions
+thetas = np.array([[-40], [0.25] ]) #theta0, theta1 
+predictions = np.dot(X, thetas)
